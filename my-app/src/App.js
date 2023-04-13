131039e7d1c4b6ec234fa20b6cb3207c21461a40
Alina-Ijaz <alinaijaz073@gmail.com>
2023-04-13 13:33:52 +0500

    Add menu

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import Div1 from './Components/Div1';
+import Menu from './Components/Menu';
+
 function App() {
   return (
     <>
+    <Menu />
     <Div1 />
     </>
   );
